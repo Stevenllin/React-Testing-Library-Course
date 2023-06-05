35333fc9b82f7c429b77041ba97d3cee0479dad0
Steven <stevenlin3263@gmail.com>
2023-06-05 08:41:12 +0800

    Describe Block

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,46 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../Header';
 
-test('should render same test passed into title prop', () => {
-  render(<Header title="My Header" />);
-  const headingElement = screen.getByText(/my header/i);
-  expect(headingElement).toBeInTheDocument();
-});
 
-// test('should render same test passed into title prop 3', () => {
-//   render(<Header title="My Header" />);
-//   const headingElement = screen.getByRole('heading', { name: "My Header" });
-//   expect(headingElement).toBeInTheDocument();
-// });
-
-// test('should render same test passed into title prop 4', () => {
-//   render(<Header title="My Header" />);
-//   const headingElement = screen.getByTitle("Header");
-//   expect(headingElement).toBeInTheDocument();
-// });
-
-// test('should render same test passed into title prop 5', () => {
-//   render(<Header title="My Header" />);
-//   const headingElement = screen.getByTestId("header-1");
-//   expect(headingElement).toBeInTheDocument();
-// });
-
-// test('should render same test passed into title prop 6', async () => {
-//   render(<Header title="My Header" />);
-//   const headingElement = await screen.findByText(/my header/i);
-//   expect(headingElement).toBeInTheDocument();
-// });
-
-// test('should render same test passed into title prop 7', async () => {
-//   render(<Header title="My Header" />);
-//   const headingElement = screen.queryByText(/dogs/i);
-//   expect(headingElement).not.toBeInTheDocument();
-// });
-
-// test('should render same test passed into title prop 8', async () => {
-//   render(<Header title="My Header" />);
-//   const headingElements = screen.getAllByRole("heading");
-//   expect(headingElements.length).toBe(2);
-// });
-
-
+describe("Header", () => {
+  test('should render same test passed into title prop', () => {
+    render(<Header title="My Header" />);
+    const headingElement = screen.getByText(/my header/i);
+    expect(headingElement).toBeInTheDocument();
+  });
+})
--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -10,26 +10,20 @@ const MockTodoFooter = ({numberOfIncompleteTasks}) => {
   )
 }
 
-test('should render same test passed into title prop 1', () => {
-  render(
-    <MockTodoFooter numberOfIncompleteTasks={5} />
-  );
-  const paragraphElement = screen.getByText(/5 tasks left/i);
-  expect(paragraphElement).toBeInTheDocument();
-});
-
-test('should render task when the number of incomplete tasks is one', () => {
-  render(
-    <MockTodoFooter numberOfIncompleteTasks={1} />
-  );
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toHaveTextContent("1 task left");
-});
-
-test('should render task when the number of incomplete tasks is one 1', () => {
-  render(
-    <MockTodoFooter numberOfIncompleteTasks={1} />
-  );
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement.textContent).toBe("1 task left");
-});
+describe("TodoFooter", () => {
+  test('should render same test passed into title prop 1', () => {
+    render(
+      <MockTodoFooter numberOfIncompleteTasks={5} />
+    );
+    const paragraphElement = screen.getByText(/5 tasks left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
+  
+  test('should render task when the number of incomplete tasks is one', () => {
+    render(
+      <MockTodoFooter numberOfIncompleteTasks={1} />
+    );
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toHaveTextContent("1 task left");
+  });
+})
